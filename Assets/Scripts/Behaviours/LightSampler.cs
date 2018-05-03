04a0718358fc778c5711c147e26480ec636f5b72
Daniel Everland <danieleverland@gmail.com>
2018-05-03 17:46:00 +0200

    Created sampler behaviour

---
+++ Assets/Scripts/Behaviours/LightSampler.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSampler : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 _center;
+    [SerializeField]
+    private Vector2 _size = new Vector2(10, 10);
+    [SerializeField]
+    private bool _overrideCenter = true;
+}
---
+++ Assets/Scripts/Editor/LightSamplerEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LightSampler))]
+[CanEditMultipleObjects()]
+public class LightSamplerEditor : Editor {
+
+    private SerializedProperty _sizeProperty;
+    private SerializedProperty _overrideCenter;
+    private SerializedProperty _centerProperty;
+
+    private const string TOOLTIP_OVERRIDE_CENTER = "Use a value rather than the position of the transform";
+    
+    public override void OnInspectorGUI()
+    {
+        DrawBoundsProperties();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+    private void DrawBoundsProperties()
+    {
+        EditorGUILayout.PropertyField(_overrideCenter);
+        EditorGUILayout.PropertyField(_sizeProperty);
+
+        if (_overrideCenter.boolValue)
+        {
+            EditorGUI.indentLevel++;
+
+            EditorGUILayout.PropertyField(_centerProperty);
+
+            EditorGUI.indentLevel--;
+        }
+    }
+    private void OnEnable()
+    {
+        CreatePropertyReferences();
+    }
+    private void CreatePropertyReferences()
+    {
+        _centerProperty = serializedObject.FindProperty("_center");
+        _sizeProperty = serializedObject.FindProperty("_size");
+        _overrideCenter = serializedObject.FindProperty("_overrideCenter");
+    }
+}
