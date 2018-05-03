850866bfbc909c28103208ffeaf2592450d6263a
Daniel Everland <danieleverland@gmail.com>
2018-05-03 17:35:17 +0200

    Created some fundamentals

---
+++ Assets/Scripts/Managers/LightSamplingManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSamplingManager : MonoBehaviour {
+
+    public static event System.Action OnBeforeSample;
+    public static event System.Action OnSample;
+    public static event System.Action OnAfterSample;
+
+    /// <summary>
+    /// Time in seconds between every sampling update
+    /// </summary>
+    public const float UPDATE_INTERVAL = 0.1f;
+
+    private float _time;
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        
+        if(_time >= UPDATE_INTERVAL)
+        {
+            OnBeforeSample();
+            OnSample();
+            OnAfterSample();
+        }
+    }
+}
---
+++ Managers/LightSamplingManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scripts.Managers
+{
+    public class LightSamplingManager
+    {
+    }
+}
