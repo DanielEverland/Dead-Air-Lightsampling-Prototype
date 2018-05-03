be9fa17e70398f5dcd10c1c8f7cc38b91693a2cb
Daniel Everland <danieleverland@gmail.com>
2018-05-03 18:40:47 +0200

    Drew handles for sampler bounds

--- Assets/Scripts/Behaviours/LightSampler.cs
+++ Assets/Scripts/Behaviours/LightSampler.cs
@@ -11,4 +11,16 @@ public class LightSampler : MonoBehaviour {
     private Vector2 _size = new Vector2(10, 10);
     [SerializeField]
     private bool _overrideCenter = true;
+    
+    public Vector2 Size { get { return _size; } }
+    public Vector2 Center
+    {
+        get
+        {
+            if (_overrideCenter)
+                return _center;
+
+            return transform.position;
+        }
+    }
 }
--- Assets/Scripts/Editor/LightSamplerEditor.cs
+++ Assets/Scripts/Editor/LightSamplerEditor.cs
@@ -8,16 +8,21 @@ using UnityEditor;
 [CanEditMultipleObjects()]
 public class LightSamplerEditor : Editor {
 
+    private LightSampler Target { get { return (LightSampler)target; } }
+
     private SerializedProperty _sizeProperty;
     private SerializedProperty _overrideCenter;
     private SerializedProperty _centerProperty;
+
+    private readonly Color _primaryColor = new Color(0, 1, 1, 0.1f);
+    private readonly Color _primaryColorOpaque = new Color(0, 1, 1, 1);
 
     private const string TOOLTIP_OVERRIDE_CENTER = "Use a value rather than the position of the transform";
     
     public override void OnInspectorGUI()
     {
         DrawBoundsProperties();
-
+        
         serializedObject.ApplyModifiedProperties();
     }
     private void DrawBoundsProperties()
@@ -37,6 +42,20 @@ public class LightSamplerEditor : Editor {
     private void OnEnable()
     {
         CreatePropertyReferences();
+    }
+    private void OnSceneGUI()
+    {
+        DrawHandles();
+    }
+    private void DrawHandles()
+    {
+        Rect rect = new Rect()
+        {
+            center = Target.Center - Target.Size / 2,
+            size = Target.Size,
+        };
+        
+        Handles.DrawSolidRectangleWithOutline(rect, _primaryColor, _primaryColorOpaque);
     }
     private void CreatePropertyReferences()
     {
