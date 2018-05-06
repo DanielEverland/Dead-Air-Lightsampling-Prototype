de9d4e0ffa2c78c668efcb09e68b0e1a2073a74b
Daniel Everland <danieleverland@gmail.com>
2018-05-06 22:34:02 +0200

    Added spotlight sampling

---
+++ Assets/Scripts/Core/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathHelper {
+
+    public static bool IsWithinCone(Vector3 point, Vector3 coneStartPoint, Vector3 direction, float distance, float coneAngle)
+    {
+        Plane plane = new Plane(direction, coneStartPoint);
+        Vector3 pointOnPlane = plane.ClosestPointOnPlane(point);
+        Vector3 pointOnPlaneDelta = point - pointOnPlane;
+        
+        float distanceFromPlane = pointOnPlaneDelta.magnitude;
+        float circleSizeAtDistance = distanceFromPlane * Mathf.Tan(Mathf.Deg2Rad * coneAngle / 2.0f);
+
+        Vector3 pointAlongAxis = coneStartPoint + pointOnPlaneDelta.normalized * distanceFromPlane;
+        
+        float distanceToPointAlongAxis = Vector3.Distance(point, pointAlongAxis);
+
+        return distanceToPointAlongAxis < circleSizeAtDistance;
+    }
+}
--- Assets/Scripts/Managers/LightSampling.cs
+++ Assets/Scripts/Managers/LightSampling.cs
@@ -32,9 +32,20 @@ public static class LightSampling {
                 return GetIntensityFromPointLight(light, point);
             case LightType.Directional:
                 return GetIntensityFromDirectionalLight(light, point);
+            case LightType.Spot:
+                return GetIntensityFromSpotLight(light, point);
             default:
                 throw new System.NotImplementedException();
         }
+    }
+    private static float GetIntensityFromSpotLight(Light light, Vector3 point)
+    {
+        if (light.type != LightType.Spot)
+            throw new System.ArgumentException();
+
+        float attenuation = QuadraticAttenuationFalloff(light.transform.position, point, light.range) * light.intensity;
+
+        return attenuation * GetRadialFalloff(point, light.transform.position, light.transform.forward, light.range, light.spotAngle);
     }
     private static float GetIntensityFromDirectionalLight(Light light, Vector3 point)
     {
@@ -55,7 +66,7 @@ public static class LightSampling {
         switch (light.type)
         {
             case LightType.Spot:
-                return IsPointVisible(light.transform.position, point);
+                return IsVisibleFromSpotlight(point, light);
             case LightType.Directional:
                 return IsDirectionVisible(point, light.transform.forward);
             case LightType.Point:
@@ -63,6 +74,13 @@ public static class LightSampling {
             default:
                 throw new System.NotImplementedException();
         }
+    }
+    private static bool IsVisibleFromSpotlight(Vector3 point, Light light)
+    {
+        if (!MathHelper.IsWithinCone(point, light.transform.position, light.transform.forward, light.range, light.spotAngle))
+            return false;
+
+        return IsDirectionVisible(point, light.transform.forward);
     }
     private static bool IsDirectionVisible(Vector3 from, Vector3 direction)
     {
@@ -73,6 +91,24 @@ public static class LightSampling {
         Vector3 delta = from - to;
 
         return !Physics.Raycast(from, -delta.normalized, delta.magnitude);
+    }
+    private static float GetRadialFalloff(Vector3 point, Vector3 coneStartPoint, Vector3 direction, float distance, float coneAngle)
+    {
+        Plane plane = new Plane(direction, coneStartPoint);
+        Vector3 pointOnPlane = plane.ClosestPointOnPlane(point);
+        Vector3 pointOnPlaneDelta = point - pointOnPlane;
+
+        float distanceFromPlane = pointOnPlaneDelta.magnitude;
+        float circleSizeAtDistance = distanceFromPlane * Mathf.Tan(Mathf.Deg2Rad * coneAngle / 2.0f);
+
+        Vector3 pointAlongAxis = coneStartPoint + pointOnPlaneDelta.normalized * distanceFromPlane;
+
+        float distanceToPointAlongAxis = Vector3.Distance(point, pointAlongAxis);
+        
+        float interpolator = 1 - Mathf.InverseLerp(0, circleSizeAtDistance * 0.9f, distanceToPointAlongAxis);
+        interpolator = Mathf.Sqrt(interpolator) * 1.2f;
+        
+        return Mathf.Clamp(interpolator, 0, 1);
     }
     private static float QuadraticAttenuationFalloff(Vector3 a, Vector3 b, float range)
     {
